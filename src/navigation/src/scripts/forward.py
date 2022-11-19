2d2c5c3a2d49f747eb2ca74a6b978133487021b5
L0Lmaker <L0Lmaker@users.noreply.github.com>
2022-11-19 03:44:18 +0000

    autopep8 action fixes

--- src/navigation/src/scripts/forward.py
+++ src/navigation/src/scripts/forward.py
@@ -3,6 +3,7 @@
 import rospy
 from std_msgs.msg import Int32, Float32
 
+
 class Forward(object):
 
     def __init__(self):
@@ -14,18 +15,19 @@ class Forward(object):
     def publish(self):
         pub_speed = rospy.Publisher('speed', Float32, queue_size=1)
         pub_angle = rospy.Publisher('angle', Float32, queue_size=1)
-    
+
         rospy.Subscriber("state", Int32, self.state_callback)
 
         rospy.init_node('forward', anonymous=True)
-        rate = rospy.Rate(10) # 10hz
+        rate = rospy.Rate(10)  # 10hz
         while not rospy.is_shutdown():
             print(self.state)
             if (self.state == 1):
-                pub_speed.publish(1.0) # 1 is maximum, -1 is maximum reverse, 0 is stop
-                pub_angle.publish(0.0) # negative is left, positive is right. Max is 1.0.
-        
+                pub_speed.publish(1.0)  # 1 is maximum, -1 is maximum reverse, 0 is stop
+                pub_angle.publish(0.0)  # negative is left, positive is right. Max is 1.0.
+
             rate.sleep()
+
 
 if __name__ == '__main__':
     try:
@@ -33,4 +35,3 @@ if __name__ == '__main__':
         forward_node.publish()
     except rospy.ROSInterruptException:
         pass
-
