74ea89057e8e4f0b1c9cf6fbe41f59b7e0a03fd2
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-02-04 12:42:06 -0800

    Removed LoggingSeverity enum due to errors

--- src/helper_scripts/node_setup_helper.py
+++ src/helper_scripts/node_setup_helper.py
@@ -1,6 +1,5 @@
 from rclpy.node import Node
 from rcl_interfaces.msg import ParameterDescriptor
-from rclpy.impl.logging_severity import LoggingSeverity
 
 
 '''
@@ -12,12 +11,12 @@ def node_setup_helper(node: Node):
     node.declare_parameters(
         namespace='',
         parameters=[
-            ('verbosity', LoggingSeverity.INFO.value, ParameterDescriptor(
+            ('verbosity', 20, ParameterDescriptor(
                 description='Verbosity log messages: UNSET. DEBUG, INFO, WARN, ERROR and FATAL in ascending order.' +
                 'a logger will only process log messages with severity at or higher than a specified level chosen for the logger.')
-             )
+            )
         ]
     )
     logger_level: int = node.get_parameter('verbosity').value
-    node.get_logger().info(f'Setting logger verbosity to {str(LoggingSeverity(logger_level))}')
+    node.get_logger().info(f'Setting logger verbosity to {str(logger_level)}')
     node.get_logger().set_level(logger_level)
