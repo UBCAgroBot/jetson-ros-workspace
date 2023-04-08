0c297190115927c0c4c3679e7d43c4015f4313fc
alibouz <alibouz@Ubuntu20.04.myguest.virtualbox.org>
2023-04-08 11:22:17 -0700

    Model code added
    Model code added
    
    model file removed (it was too big to push)

--- src/image_rec/image_rec/frcnn.py
+++ src/image_rec/image_rec/frcnn.py
@@ -5,6 +5,14 @@ import cv2
 from sensor_msgs.msg import Image
 from vision_msgs.msg import BoundingBox2D, BoundingBox2DArray
 from std_msgs.msg import Header
+import numpy as np
+
+import torchvision
+import torch
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+NUM_CLASSES = 4
+RESIZE_TO = 512
 
 
 class FrcnnNode(Node):
@@ -16,16 +24,31 @@ class FrcnnNode(Node):
         self.sub = self.create_subscription(Image, 'image_rec/pre_processed_image', self.callback, 10)
         self.bridge = CvBridge()
 
+        # set the computation device
+        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        # load the model and the trained weights
+        self.model = create_model(num_classes=NUM_CLASSES).to(device)
+        self.model.load_state_dict(torch.load(
+            'src/image_rec/models/model13.pth', map_location=device
+        ))
+        self.model.eval()
+
     def callback(self, data):
         try:
             image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
 
-        #TODO: Run FRCNN on image, get weed bounding boxes
+        image = process_image(image)
+        preds = self.model(image)
+
+        weed_preds = []
+
+        for label, box in zip(preds[0]['labels'], preds[0]['boxes']):
+            if label == 'weed':
+                weed_preds.append(box.detach().numpy().tolist())
 
         #convert weed bounding boxes to ROS message
-        weed_preds = [[1,2,3,4], [5,6,7,8]]
         weed_preds = [convert_edgebox_to_cbox(bbox) for bbox in weed_preds]
         weed_preds_msg = bbox_list_to_msg(weed_preds)
 
@@ -35,6 +58,17 @@ class FrcnnNode(Node):
         #publish
         self.pub.publish(weed_preds_msg)
 
+
+def process_image(image):
+    """Pre-processes image for FRCNN"""
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (RESIZE_TO, RESIZE_TO))
+    image = image.transpose(2, 0, 1).astype(np.float32)
+    image = image / 255.0
+    image = torch.from_numpy(image)
+    image = image.float()
+    image = image.unsqueeze(0)
+    return image
 
 def convert_edgebox_to_cbox(edgebox):
     """Converts bounding box from [xmin, ymin, xmax, ymax] format 
@@ -61,6 +95,30 @@ def bbox_list_to_msg(bbox_list):
     return bbox_arr_msg
 
 
+def create_model(num_classes):
+    
+    # load Faster RCNN pre-trained model
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False)
+    
+    # get the number of input features 
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # define a new head for the detector with required number of classes
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes) 
+
+    """
+    for param in model.parameters():
+        param.requires_grad = False
+        try:
+            num_ftrs = model.fc.in_features
+        except:
+            num_ftrs = model.classifier[0].in_features
+        # Here the size of each output sample is set to 2.
+        # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
+        model.fc = nn.Linear(num_ftrs, NUM_CLASSES)
+        model= model.to(device)
+    """
+
+    return model
 
 def main(args=None):
     rclpy.init(args=args)
--- src/image_rec/image_rec/yolo.py
+++ src/image_rec/image_rec/yolo.py
@@ -5,6 +5,11 @@ import cv2
 from sensor_msgs.msg import Image
 from vision_msgs.msg import BoundingBox2D, BoundingBox2DArray
 from std_msgs.msg import Header
+import torch
+import onnxruntime as ort
+import numpy as np
+
+RESIZE_TO = 512
 
 
 class YoloNode(Node):
@@ -12,9 +17,13 @@ class YoloNode(Node):
     def __init__(self):
         super().__init__('yolo')
         self.get_logger().info("YoloNode node has been started")
-        self.pub = self.create_publisher(Image, 'image_rec/yolo_prediction', 10)
+        self.pub = self.create_publisher(BoundingBox2DArray, 'image_rec/yolo_prediction', 10)
         self.sub = self.create_subscription(Image, 'image_rec/pre_processed_image', self.callback, 10)
         self.bridge = CvBridge()
+
+        self.ort_sess = ort.InferenceSession('model.onnx')
+        self.input_name = self.ort_sess.get_inputs()[0].name
+        self.output_name = self.ort_sess.get_outputs()[0].name
 
     def callback(self, data):
         try:
@@ -22,7 +31,10 @@ class YoloNode(Node):
         except CvBridgeError as e:
             print(e)
 
+        image = process_image(image)
+
         #TODO: Run YOLO on image, get weed bounding boxes
+        output = self.ort_sess.run([self.output_name], {self.input_name: image})[0]
 
         #convert weed bounding boxes to ROS message
         weed_preds = [[1,2,3,4], [5,6,7,8]]
@@ -35,6 +47,18 @@ class YoloNode(Node):
         self.pub.publish(weed_preds_msg)
 
 
+def process_image(image):
+    #DOUBLE CHECK THIS FUNCTION
+    """Pre-processes image for YOLO"""
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (RESIZE_TO, RESIZE_TO))
+    image = image.transpose(2, 0, 1).astype(np.float32)
+    image = image / 255.0
+    image = torch.from_numpy(image)
+    image = image.float()
+    image = image.unsqueeze(0)
+    return image
+
 def bbox_list_to_msg(bbox_list):
     """Converts list of bounding boxes to ROS message.
         Must be in [xcenter, ycenter, width, height] format"""
@@ -43,10 +67,10 @@ def bbox_list_to_msg(bbox_list):
         bbox_msg = BoundingBox2D()
         bbox_msg.center.x = bbox[0]
         bbox_msg.center.y = bbox[1]
-        bbox_msg.size_x = bbox[2]
-        bbox_msg.size_y = bbox[3]
+        bbox_msg.size_x = float(bbox[2])
+        bbox_msg.size_y = float(bbox[3])
 
-        bbox_arr_msg.bounding_boxes.append(bbox_msg)
+        bbox_arr_msg.boxes.append(bbox_msg)
 
     return bbox_arr_msg
 
