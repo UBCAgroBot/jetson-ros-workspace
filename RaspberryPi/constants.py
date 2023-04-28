f3cc4f95759e9b54ceac7a6b6ee0b13e06cd571b
ArnoldYing <ArnoldYing@users.noreply.github.com>
2023-04-28 20:36:43 +0000

    autopep8 action fixes

--- RaspberryPi/constants.py
+++ RaspberryPi/constants.py
@@ -1,44 +1,49 @@
 from enum import Enum
 
 # LEFT and RIGHT output values for stepper motor
-DIR_RIGHT = 1;
-DIR_LEFT = 0;
+DIR_RIGHT = 1
+DIR_LEFT = 0
 
 # FORWARD and BACKWARD output values for gearbox motor
-DIR_FORWARD = 1;
-DIR_BACKWARD = 0;
+DIR_FORWARD = 1
+DIR_BACKWARD = 0
 
 # Angular Speed, recommended setting 7, DO NOT DECREASE TO < 3
-ANGULAR_SPEED = 7 / 1000; # python sleep uses seconds
+ANGULAR_SPEED = 7 / 1000  # python sleep uses seconds
 
 # Strength of PWM, between 0 and 255, higher is faster
-PWM_SPEED = 80; # % duty cycle
-PWM_MAX_SPEED = 100;
-PWM_FREQUENCY = 100;
+PWM_SPEED = 80  # % duty cycle
+PWM_MAX_SPEED = 100
+PWM_FREQUENCY = 100
 
 # 1 pulse to angle
 # 400 revs per 360 degrees
-REV_ANGLE = 360.0 / 400.0;
+REV_ANGLE = 360.0 / 400.0
 
 # max turning angle
-MAX_ANGLE = 90.0;
+MAX_ANGLE = 90.0
 
 # enum for vertical directions
+
+
 class V_Directions(Enum):
     halted = 0
     forward = 1
     backward = 2
 
 # enum for horizontal directions
+
+
 class H_Directions(Enum):
     straight = 0
     left = 1
     right = 2
 
+
 # level 0: once every DEBUG_FREQUENCY seconds
 # level 1: no timing restrictions
 DEBUG_LEVEL = 1
-DEBUG_FREQUNCY = 1 # seconds
+DEBUG_FREQUNCY = 1  # seconds
 
 '''HARDWARD PWM'''
 # from rpi_hardware_pwm import HardwarePWM
@@ -61,37 +66,37 @@ DEBUG_FREQUNCY = 1 # seconds
 # D: DC Motor Driver
 # Each wheel uses a DC Motor controlled by a DC Motor Driver
 # the front two wheels uses a Gearbox connection to control the stepper motor for direction
-DFL_DIR = 11;
-DFL_PWM = 13;
-DFR_DIR = 15;
-DFR_PWM = 16;
-DBL_DIR = 18;
-DBL_PWM = 22;
-DBR_DIR = 29;
-DBR_PWM = 31;
-GL_DIR = 32;
-GL_PULSE = 33;
-GR_DIR = 36;
-GR_PULSE = 37;
+DFL_DIR = 11
+DFL_PWM = 13
+DFR_DIR = 15
+DFR_PWM = 16
+DBL_DIR = 18
+DBL_PWM = 22
+DBR_DIR = 29
+DBR_PWM = 31
+GL_DIR = 32
+GL_PULSE = 33
+GR_DIR = 36
+GR_PULSE = 37
 
 PINS = (
-  DFL_DIR,
-  DFL_PWM,
-  DFR_DIR,
-  DFR_PWM,
-  DBL_DIR,
-  DBL_PWM,
-  DBR_DIR,
-  DBR_PWM,
-  GL_DIR,
-  GL_PULSE,
-  GR_DIR,
-  GR_PULSE,
+    DFL_DIR,
+    DFL_PWM,
+    DFR_DIR,
+    DFR_PWM,
+    DBL_DIR,
+    DBL_PWM,
+    DBR_DIR,
+    DBR_PWM,
+    GL_DIR,
+    GL_PULSE,
+    GR_DIR,
+    GR_PULSE,
 )
 
 PWMS = (
-  DFL_PWM,
-  DFR_PWM,
-  DBL_PWM,
-  DBR_PWM,
+    DFL_PWM,
+    DFR_PWM,
+    DBL_PWM,
+    DBR_PWM,
 )
--- RaspberryPi/rpi_helper.py
+++ RaspberryPi/rpi_helper.py
@@ -2,37 +2,45 @@ import RPi.GPIO as GPIO
 from time import sleep, time
 from constants import *
 
-## turns stepper motor in desired direction
+# turns stepper motor in desired direction
+
+
 def turn_wheels(direction):
-  GPIO.output(GL_DIR, direction)
-  GPIO.output(GR_DIR, direction)
-  generate_pulse()
+    GPIO.output(GL_DIR, direction)
+    GPIO.output(GR_DIR, direction)
+    generate_pulse()
 
-## generates pulse for stepper motors
+# generates pulse for stepper motors
+
+
 def generate_pulse():
-  GPIO.output(GL_PULSE, GPIO.HIGH)
-  GPIO.output(GR_PULSE, GPIO.HIGH)
-  sleep(ANGULAR_SPEED)
-  GPIO.output(GL_PULSE, GPIO.LOW)
-  GPIO.output(GR_PULSE, GPIO.LOW)
-  sleep(ANGULAR_SPEED)
-    
+    GPIO.output(GL_PULSE, GPIO.HIGH)
+    GPIO.output(GR_PULSE, GPIO.HIGH)
+    sleep(ANGULAR_SPEED)
+    GPIO.output(GL_PULSE, GPIO.LOW)
+    GPIO.output(GR_PULSE, GPIO.LOW)
+    sleep(ANGULAR_SPEED)
 
-## turns gearbox motor in desired direction
+
+# turns gearbox motor in desired direction
 def rotate_wheels(direction, pwm_controls):
-  GPIO.output(DFL_DIR, int(direction))
-  GPIO.output(DFR_DIR, int(not direction))
-  GPIO.output(DBL_DIR, int(direction))
-  GPIO.output(DBR_DIR, int(not direction))
+    GPIO.output(DFL_DIR, int(direction))
+    GPIO.output(DFR_DIR, int(not direction))
+    GPIO.output(DBL_DIR, int(direction))
+    GPIO.output(DBR_DIR, int(not direction))
 
-  generate_pwm(PWM_SPEED, pwm_controls)
+    generate_pwm(PWM_SPEED, pwm_controls)
 
-## generate pwm for gearbox motors
+# generate pwm for gearbox motors
+
+
 def generate_pwm(speed, pwm_controls):
-  for pwmc in pwm_controls:
-    pwmc.ChangeDutyCycle(int((speed // 10) * 10))
+    for pwmc in pwm_controls:
+        pwmc.ChangeDutyCycle(int((speed // 10) * 10))
 
 # Resets the stepper motor angle to 0 degrees
+
+
 def reset_angle(current_angle):
     dir = DIR_RIGHT if current_angle < 0 else DIR_LEFT
     inc_value = REV_ANGLE if current_angle < 0 else -REV_ANGLE
@@ -46,22 +54,25 @@ def reset_angle(current_angle):
 
     return current_angle
 
-def setup_rpi(pwm_controls):
-  GPIO.setwarnings(False)			#disable warnings
-  GPIO.setmode(GPIO.BOARD)		#set pin numbering system
-  GPIO.setup(PINS,GPIO.OUT)
 
-  for pwm in PWMS:
-    pwm_control = GPIO.PWM(pwm, PWM_FREQUENCY) # create PWM instance with frequency
-    pwm_control.start(0) # set 0 Duty Cycle 
-    pwm_controls.append(pwm_control)
+def setup_rpi(pwm_controls):
+    GPIO.setwarnings(False)  # disable warnings
+    GPIO.setmode(GPIO.BOARD)  # set pin numbering system
+    GPIO.setup(PINS, GPIO.OUT)
+
+    for pwm in PWMS:
+        pwm_control = GPIO.PWM(pwm, PWM_FREQUENCY)  # create PWM instance with frequency
+        pwm_control.start(0)  # set 0 Duty Cycle
+        pwm_controls.append(pwm_control)
+
 
 last_debug_print_time = 0
+
+
 def debug_print(*output: object):
     if DEBUG_LEVEL == 0:
-      if time() - last_debug_print_time > DEBUG_FREQUNCY:
-        print(output)
-        last_debug_print_time = time()
+        if time() - last_debug_print_time > DEBUG_FREQUNCY:
+            print(output)
+            last_debug_print_time = time()
     elif DEBUG_LEVEL == 1:
-      print(output)
-   
+        print(output)
