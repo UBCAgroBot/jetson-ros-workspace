b64a7430754e7957e3c5e24ba85b72e4bf2835fb
davidw0311 <davidw0311@gmail.com>
2023-04-27 19:30:08 -0700

    fixed overlay node

--- src/image_rec/image_rec/weed_id_assigner.py
+++ src/image_rec/image_rec/weed_id_assigner.py
@@ -13,7 +13,7 @@ class WeedIDAssigner(Node):
         self.declare_parameter('area_ratio_threshold_lower', 0.95)
         self.declare_parameter('area_ratio_threshold_upper', 1.05)
         self.bounding_boxes = {}
-        self.publisher_ = self.create_publisher(WeedBoundingBox, 'weed_bounding_boxes', 10)
+        self.publisher_ = self.create_publisher(WeedBoundingBox, 'image_rec/weed_bounding_boxes', 10)
         self.subscription = self.create_subscription(
             BoundingBox2DArray,
             'image_rec/frcnn_prediction',
@@ -33,22 +33,24 @@ class WeedIDAssigner(Node):
             obj_id = self.get_id_for_box((center_x, center_y), area)
             weed_bounding_box = WeedBoundingBox()
             weed_bounding_box.header = header
-            weed_bounding_box.center.x = center_x
-            weed_bounding_box.center.y = center_y
+            weed_bounding_box.center_x = center_x
+            weed_bounding_box.center_y = center_y
             weed_bounding_box.size_x = box.size_x
             weed_bounding_box.size_y = box.size_y
             weed_bounding_box.obj_id = obj_id
             weed_bounding_boxes.append(weed_bounding_box)
-        self.publisher_.publish(weed_bounding_boxes)
+            self.publisher_.publish(weed_bounding_box)
 
     def get_id_for_box(self, center, area):
         for obj_id, (prev_center, prev_area, timestamp) in self.bounding_boxes.items():
             distance = math.sqrt((center[0] - prev_center[0])**2 + (center[1] - prev_center[1])**2)
             area_ratio = area / prev_area
-            if distance < self.get_parameter('distance_threshold').value and area_ratio > self.get_parameter('area_ratio_threshold_lower').value and area_ratio < self.get_parameter('area_ratio_threshold_upper'):
+
+            if distance < self.get_parameter('distance_threshold').value and area_ratio > self.get_parameter('area_ratio_threshold_lower').value and area_ratio < self.get_parameter('area_ratio_threshold_upper').value:
                 self.bounding_boxes[obj_id] = (center, area, self.get_clock().now())
                 return obj_id
-        new_id = max(self.bounding_boxes.keys(), default=-1) + 1
+        new_id = int(max(self.bounding_boxes.keys(), default=0) + 1)
+        self.get_logger().info(str(new_id))
         self.bounding_boxes[new_id] = (center, area, self.get_clock().now())
         return new_id
 
@@ -57,7 +59,7 @@ class WeedIDAssigner(Node):
         to_delete = []
         for obj_id, (_, _, timestamp) in self.bounding_boxes.items():
             time_diff = now - timestamp
-            if time_diff.nanoseconds > 2000000000:  # two seconds
+            if time_diff.nanoseconds > 20000000000:  # twenty seconds
                 to_delete.append(obj_id)
         for obj_id in to_delete:
             del self.bounding_boxes[obj_id]
